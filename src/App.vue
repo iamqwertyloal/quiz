7332f39a6b2da188391387ffe87cef144d5f9a23
iamqwertyloal <iamqwertyloal@gmail.com>
2023-01-20 21:40:20 +0500

    Add questions rendering with props and directives

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="ctr">
-    <questions></questions>
-    <result></result>
+    <questions v-if="questionsAnswered < questions.length"
+    :questions="questions"
+    />
+    <result v-else />
     <button type="button" class="reset-btn">Reset</button>
 </div>
 </template>
@@ -18,6 +20,7 @@ export default {
   },
   data() {
     return {
+    questionsAnswered: 0,
     questions: [
         {
             q: 'What is 2 + 2?', 
